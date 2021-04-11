92b8856e7c9365fbaf8b3597389365d4d631e43a
xNakero <thenakero@hotmail.com>
2021-04-11 14:18:02 +0200

    Initial commit

---
+++ src/main/java/pl/pz/oszczedzator3000/Oszczedzator3000Application.java
@@ -0,0 +1,13 @@
+package pl.pz.oszczedzator3000;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oszczedzator3000Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oszczedzator3000Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/pz/oszczedzator3000/Oszczedzator3000ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pz.oszczedzator3000;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oszczedzator3000ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
